70acef6529d62ad9fba69663ae60b01668e616b0
Managementaccount <97501211+Managementaccount@users.noreply.github.com>
2022-06-28 12:21:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 import discord
 
 # this is my Hugging Face profile link
-API_URL = 'https://api-inference.huggingface.co/models/r3dhummingbird/'
+API_URL = 'https://github.com/Managementaccount/tbnr_the_bot'
 
 class MyClient(discord.Client):
     def __init__(self, model_name):
